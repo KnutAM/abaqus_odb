9d9a851cc79e430f13dfa195c887ed5ed9b2210c
Knut Andreas Meyer <knutam@gmail.com>
2020-12-13 15:15:15 +0100

    Fixed bugs and added feature for set and multiple node extraction with single variable

--- odb_scripts/node_data.py
+++ odb_scripts/node_data.py
@@ -7,10 +7,107 @@ from abaqusConstants import *
 from abaqus import session
 import visualization
 
-def get(odb, inst_name, position, variables, step_numbers, increments=[':'], 
-        tol=1.e-2):
-    """ Get given variables from odb at given positions for specified steps and 
+
+def get_multiple_positions(odb, inst_name, positions, variable, step_numbers, 
+                           increments=['0:-1'], tol=1.e-2):
+    """ Get given variable from odb at given positions for specified steps and 
     increments
+    
+    :param odb: The odb object to extract results from
+    :type odb: Odb object (Abaqus)
+    
+    :param inst_name: The name of the instance to get results for
+    :type inst_name: str
+    
+    :param position: Node coordinates where results will be extracted. 
+                     Alternatively, name of set in odb instance 
+                     containing nodes from which results shall be 
+                     extracted. 
+    :type position: list[ list[ float ] ] or str
+    
+    :param variable: Variable to extract
+    :type variable: str
+    
+    :param step_numbers: List of step numbers from which to extract results
+    :type step_numbers: list[ int ]
+    
+    :param increments: List of increments from which to extract results. 
+                       ['0:-1'] implies all increments. 
+                       Note that python negative numbering can be used, 
+                       such that -1 implies last increment. Opposed to 
+                       python lists, the last given index is included.
+    :type increments: list[ int ]
+    
+    :param tol: Tolerance for node position
+    :type tol: float
+    
+    :returns: Dictionary describing the results with fields containing 
+              numpy arrays. Each row describe new time points
+              
+              - "step"
+              - "incr"
+              - "time"
+              - "node"
+              - 0
+              - 1
+              - ...
+              - N-1
+              
+              Where the number 0-(N-1) is the index in the position list
+              describing the node. The "node" entry is a Nx3 array with 
+              the node coordinates. 
+              
+    :rtype: dict
+    
+    """
+    
+    odb_inst = odb.rootAssembly.instances[inst_name]
+    
+    # Get node labels based on the positions
+    if isinstance(positions, str):
+        node_labels = [n.label for n in odb_inst.nodeSets[positions].nodes]
+    else:
+        node_labels = get_node_labels(odb, inst_name, positions, tol)
+    
+    node_spec = ((inst_name, tuple(node_labels)),)
+    
+    # Translate the var list to the variable list format 
+    # requried by xyDataListFromField
+    variable_list = get_variable_list([variable])
+    
+    # Set the active frames
+    step_data, incr_data = set_active_frames(odb, step_numbers, increments)
+    
+    # Get xy_data_list
+    # Need to set odb active, otherwise the xyDataListFromField will fail!
+    viewport = session.viewports[session.viewports.keys()[0]]
+    viewport.setValues(displayedObject=odb)
+    xy_data_list = session.xyDataListFromField(odb=odb, 
+                                               outputPosition=NODAL,
+                                               variable=variable_list, 
+                                               nodeLabels=node_spec)
+    
+    node_data = {'step': step_data,
+                 'incr': incr_data,
+                 'node': []}
+    
+    
+    for node_ind, xy_data in enumerate(xy_data_list):
+        data = np.array(xy_data.data)
+        if not 'time' in node_data:
+            node_data['time'] = data[:,0]
+        node_data[node_ind] = data[:,1]
+        node_data['node'].append(odb_inst.nodes[node_labels[node_ind]].coordinates)
+        
+    node_data['node'] = np.array(node_data['node'])
+    
+    return node_data
+    
+
+def get_multiple_variables(odb, inst_name, position, variables, step_numbers,
+                           increments=['0:-1'], tol=1.e-2):
+    """ Get given variables from odb at given position for specified 
+    steps and increments
     
     :param odb: The odb object to extract results from
     :type odb: Odb object (Abaqus)
@@ -28,9 +125,10 @@ def get(odb, inst_name, position, variables, step_numbers, increments=[':'],
     :type step_numbers: list[ int ]
     
     :param increments: List of increments from which to extract results. 
-                       [':'] implies all increments. 
+                       ['0:-1'] implies all increments. 
                        Note that python negative numbering can be used, 
-                       such that -1 implies last increment.
+                       such that -1 implies last increment. Opposed to 
+                       python lists, the last given index is included.
     :type increments: list[ int ]
     
     :param tol: Tolerance for node position
@@ -188,16 +286,19 @@ def set_active_frames(odb, steps, incr):
     :param steps: List of step numbers to set active
     :type steps: list[ int ]
     
-    :param incr: List of increments within the given steps
-                 [':'] implies all increments. 
+    :param incr: List of increments from which to extract results. 
+                 ['0:-1'] implies all increments. 
                  Note that python negative numbering can be used, 
-                 such that -1 implies last increment.
-    :type incr: list[ int ]
+                 such that -1 implies last increment. Opposed to 
+                 python lists, the last given index is included.
+    :type incr: list[ int or str ]
     
     :returns: lists of step numbers and increments that are set to active. 
               The length is equal to the number of active frames.
     :rtype: (list, list)
     """
+    
+    
     
     all_step_names = odb.steps.keys()
     active_step_names = [all_step_names[step] for step in steps]
@@ -206,8 +307,15 @@ def set_active_frames(odb, steps, incr):
                                      for step_name in active_step_names])
     step_data = []
     incr_data = []
-    for step in steps:
-        for the_incr in incr:
+    for step, step_spec in enumerate(odb_data.activeFrames):
+        if isinstance(step_spec[1][0], str):
+            # It is only string if all increments are included
+            increments = range(len(odb_data.steps[step_spec[0]].frames))
+        else:
+            # Otherwise, a list of increments is given.
+            increments = step_spec[1]
+            
+        for the_incr in increments:
             step_data.append(step)
             incr_data.append(the_incr)
     
